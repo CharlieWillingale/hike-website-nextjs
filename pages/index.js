3c0e496c9fcb51077599e279db95863f42d0fffe
CharlieWillingale <charliegwillingale@gmail.com>
2022-10-09 09:38:20 +0100

    create util fn and test .md files

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,37 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Showcase from '../components/showcase'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts';
+
+export default function Home({allPostsData}) {
   return (
     <>
+      <Head>
+        <title>Hike Site | Home</title>
+      </Head>
       <Showcase title={'Hike.\nClimb.\nBike.'} />
+
+      <ul>
+          {allPostsData.map(() => (
+            <li key={allPostsData.id}>
+              {allPostsData.title}
+              <br />
+              {allPostsData.date}
+            </li>
+          ))}
+        </ul>
+
+
     </>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
