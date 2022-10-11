f6167e074754ebd65bb31dd7ee0247190c7ae6cc
CharlieWillingale <charliegwillingale@gmail.com>
2022-10-11 20:26:27 +0100

    Added state to burger menu and animated"

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,15 @@
 import Link from "next/link"
 import Image from "next/image"
+import { useState } from "react"
 
 function NavBar(props) {
+
+    const [isCollapsed, setIsCollapsed] = useState(true)
+
+    const onMenuClick = () => {
+        isCollapsed ? setIsCollapsed(false) : setIsCollapsed(true)    
+    }
+
     return(
         <nav className="w-full ">
 
@@ -22,10 +30,10 @@ function NavBar(props) {
                 {/* Mobile Nav */}
                 
                     {/* Burger menu */}
-                <li className="my-auto mx-5">
-                    <div className="rounded w-6 p-0.5 my-1 bg-font-grey"></div>
-                    <div className="rounded w-3 p-0.5 ml-3 bg-dark-grey"></div>
-                    <div className="rounded w-6 p-0.5 my-1 bg-font-grey"></div>
+                <li className=' my-auto mx-5  cursor-pointer' onClick={onMenuClick} >
+                    <div className={`${isCollapsed ? 'rotate-0' : 'rotate-45 top-1 relative'} transition-all rounded w-6 p-0.5 my-1 bg-font-grey`}></div>
+                    <div className={`${isCollapsed ? 'block' : 'hidden'} rounded w-3 p-0.5 ml-3 bg-dark-grey`}></div>
+                    <div className={`${isCollapsed ? 'rotate-0' : '-rotate-45 bottom-1 relative'} transition-all rounded w-6 p-0.5 my-1 bg-font-grey`}></div>
                 </li>
             </ul>
         </nav>
