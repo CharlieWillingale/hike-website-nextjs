fef1cee377f876958f59443118fe89cdf440526a
CharlieWillingale <charliegwillingale@gmail.com>
2022-10-29 13:54:59 +0100

    reverted to unoptimised

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    loader: 'akamai',
-    path: '',
+    unoptimized: true,
   },
-  assetPrefix: './',
 }
 
 module.exports = nextConfig
