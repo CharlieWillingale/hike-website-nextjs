808964a63a0af7b127783d46a9036c83d59e965e
CharlieWillingale <charliegwillingale@gmail.com>
2022-10-29 14:31:58 +0100

    Different global env attempt

--- components/card.jsx
+++ components/card.jsx
@@ -12,7 +12,7 @@ function Card(props) {
                <article className='mx-auto my-[5.5%] min-h-[93%] w-[90%] p-3 flex flex-col justify-between bg-light-grey'>
 
                     <div>
-                        <Link href={`/posts/${props.cardSlug}`} className='cursor-pointer hover:text-main-orange focus:text-main-orange'>
+                        <Link href={`${process.env.BACKEND_URL}/posts/${props.cardSlug}`} className='cursor-pointer hover:text-main-orange focus:text-main-orange'>
                             <h3 className='strong text-xl cursor-pointer hover:text-main-orange focus:text-main-orange'>{props.cardTitle}</h3>
                         </Link>
 
