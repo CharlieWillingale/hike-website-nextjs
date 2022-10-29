89207a6d9703ffbace34944f611d45e7de6a3346
CharlieWillingale <charliegwillingale@gmail.com>
2022-10-29 12:55:55 +0100

    removed downloads like for mvp

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -13,17 +13,17 @@ export default function Post({postData}) {
       <div>
         <Showcase title={postData.title}/>
         <section className='w-11/12 m-auto'>
-          <article>
+          <article className='my-5'>
             <p><span className='text-main-orange'>{postData.routeSampleText.slice(0,firstWordPos)}</span>{postData.routeSampleText.slice(firstWordPos)}</p>
-            <p className='my-3'>Skip to the <Link href='#downloads'><a className='font-bold decoration-solid text-main-orange'>gpx</a></Link></p>
+            {/* <p className='my-3'>Skip to the <Link href='#downloads'><a className='font-bold decoration-solid text-main-orange'>gpx</a></Link></p> */}
           </article>
 
           <Carousel carouselList={postData.routeCarousel}/>
 
-          <article dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-          <article id='downloads' className='my-10'>
+          <article dangerouslySetInnerHTML={{ __html: postData.contentHtml }} className='my-5'/>
+          {/* <article id='downloads' className='my-10'>
             <h3 className='text-xl'>Downloads</h3>
-          </article>
+          </article> */}
         </section>
       </div>
     );
