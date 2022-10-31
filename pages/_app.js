18d2a9e087c823f1e5fea13a42e41b3748687d4d
CharlieWillingale <charliegwillingale@gmail.com>
2022-10-31 23:25:09 +0000

    Searching for root

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
+  
+  console.log('cwd: ' + process.cwd())
+  console.log('dir name: ' + __dirname)
   return (
     <>
       <NavBar />
